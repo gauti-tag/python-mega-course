1d28bd593b5197b57b56e4f4ba8497f9f4b090d5
gautitag <gautier.tiehoule@gmail.com>
2025-03-29 11:25:36 +0000

    Refactor: Todo list - Setup if#elif replacing match#case

--- todo/main.py
+++ todo/main.py
@@ -1,60 +1,60 @@
 while True:
     # Get user input and strip space chars from it
     user_action = input("Type add, show, edit, complete or exit: ").strip()
-    match user_action:
-        # Check if user action is "add"
-        case "add": 
-            todo = input("Enter a todo: ") + "\n"
-            #Fetch the existing todos list        
-            with open('todo/todos.txt', 'r') as file_content:
-                todos = file_content.readlines()
+  
+    # Check if user action is "add"
+    if "add" in user_action: 
+        todo = user_action[4:]
+        #Fetch the existing todos list        
+        with open('todo/todos.txt', 'r') as file_content:
+            todos = file_content.readlines()
+        
+        todos.append(todo)
+                    
+        with open('todo/todos.txt', 'w') as file_content:
+            file_content.writelines(todos)
             
-            todos.append(todo)
-                        
-            with open('todo/todos.txt', 'w') as file_content:
-                file_content.writelines(todos)
-                
-        case "show" | "display":
-            #Fetch data from file    
-            with open('todo/todos.txt', 'r') as file_content:
-                todos = file_content.readlines()
+    elif "show" in user_action:
+        #Fetch data from file    
+        with open('todo/todos.txt', 'r') as file_content:
+            todos = file_content.readlines()
+        
+        # Enumerate function generate indexing for a list
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            print(f"Todo n°{index + 1}: {item.title()}")
+    elif "edit" in user_action:
+        number = int(user_action[5:])
+        number = number - 1
+        
+        with open('todo/todos.txt', 'r') as file_content:
+            todos = file_content.readlines()
             
-            # Enumerate function generate indexing for a list
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                print(f"Todo n°{index + 1}: {item.title()}")
-        case "edit":
-            number = int(input("Number of the todo to edit: "))
-            number = number - 1
+        new_todo = input("Enter new todo: ")
+        todos[number] =  new_todo + "\n"
+        
+        with open('todo/todos.txt', 'w') as file_content:
+            file_content.writelines(todos)
             
-            with open('todo/todos.txt', 'r') as file_content:
-                todos = file_content.readlines()
-                
-            new_todo = input("Enter new todo: ")
-            todos[number] =  new_todo + "\n"
+    elif "complete" in user_action:
+        number = int(user_action[9:])
+        
+        with open('todo/todos.txt', 'r') as file_content:
+            todos = file_content.readlines()
+        index = number - 1
+        todo_to_remove = todos[index].strip('\n')
+        todos.pop(index)
+        
+        with open('todo/todos.txt', 'w') as file_content:
+            file_content.writelines(todos)
             
-            with open('todo/todos.txt', 'w') as file_content:
-                file_content.writelines(todos)
-                
-        case "complete":
-            number = int(input("Number of the todo to complete: "))
-            
-            with open('todo/todos.txt', 'r') as file_content:
-                todos = file_content.readlines()
-            index = number - 1
-            todo_to_remove = todos[index].strip('\n')
-            todos.pop(index)
-            
-            with open('todo/todos.txt', 'w') as file_content:
-                file_content.writelines(todos)
-                
-            message = f"Todo {todo_to_remove} was removed from the list"
-            print(message)
-            
-        case "exit":
-            break
-        case whatever:
-            print('Hey ! you entered an unknown command')
+        message = f"Todo {todo_to_remove} was removed from the list"
+        print(message)
+        
+    elif "exit" in user_action:
+        break
+    else:
+        print("Command is not valid.")
     
 print("Bye Bye !")
     
