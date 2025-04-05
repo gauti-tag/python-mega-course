3b4ee53cc9b6e8961afb9af0230b55758d7c0fd8
gautitag <gautier.tiehoule@gmail.com>
2025-04-05 17:52:24 +0000

    Refactor: TodoList - setup function#arguments

--- todo/main.py
+++ todo/main.py
@@ -1,7 +1,13 @@
-def get_todos():
-    with open('todo/todos.txt', 'r') as file_content_local:
+def get_todos(filepath):
+    with open(filepath, 'r') as file_content_local:
         todos_local = file_content_local.readlines()
     return todos_local
+
+
+def write_todos(filpath, todos_arg):
+    with open(filpath, 'w') as file_content_local:
+        file_content_local.writelines(todos_arg)
+
 
 while True:
     # Get user input and strip space chars from it
@@ -12,16 +18,15 @@ while True:
         todo = user_action[4:]
         #Fetch the existing todos list        
         
-        todos = get_todos()
+        todos = get_todos('todo/todos.txt')
         
         todos.append(todo + '\n')
-                    
-        with open('todo/todos.txt', 'w') as file_content:
-            file_content.writelines(todos)
+        
+        write_todos('todo/todos.txt', todos)
             
     elif user_action.startswith("show"):
         #Fetch data from file    
-        todos = get_todos()
+        todos = get_todos('todo/todos.txt')
         
         # Enumerate function generate indexing for a list
         for index, item in enumerate(todos):
@@ -32,13 +37,13 @@ while True:
             number = int(user_action[5:])
             number = number - 1
             
-            todos = get_todos()
+            todos = get_todos('todo/todos.txt')
                 
             new_todo = input("Enter new todo: ")
             todos[number] =  new_todo + "\n"
             
-            with open('todo/todos.txt', 'w') as file_content:
-                file_content.writelines(todos)
+            write_todos('todo/todos.txt', todos)
+    
         except ValueError:
             print('Your command is not valid')
             continue
@@ -47,14 +52,13 @@ while True:
         try:
             number = int(user_action[9:])
             
-            todos = get_todos()
+            todos = get_todos('todo/todos.txt')
             
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
             
-            with open('todo/todos.txt', 'w') as file_content:
-                file_content.writelines(todos)
+            write_todos('todo/todos.txt', todos)
                 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
