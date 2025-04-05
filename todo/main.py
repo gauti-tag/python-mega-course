0b8c0eacf9bbdfa724ea987efa85af6f165c838d
gautitag <gautier.tiehoule@gmail.com>
2025-04-05 14:32:13 +0000

    Refactor: setup custom#function

--- todo/main.py
+++ todo/main.py
@@ -1,3 +1,8 @@
+def get_todos():
+    with open('todo/todos.txt', 'r') as file_content_local:
+        todos_local = file_content_local.readlines()
+    return todos_local
+
 while True:
     # Get user input and strip space chars from it
     user_action = input("Type add, show, edit, complete or exit: ").strip()
@@ -6,8 +11,8 @@ while True:
     if user_action.startswith("add"): 
         todo = user_action[4:]
         #Fetch the existing todos list        
-        with open('todo/todos.txt', 'r') as file_content:
-            todos = file_content.readlines()
+        
+        todos = get_todos()
         
         todos.append(todo + '\n')
                     
@@ -16,8 +21,7 @@ while True:
             
     elif user_action.startswith("show"):
         #Fetch data from file    
-        with open('todo/todos.txt', 'r') as file_content:
-            todos = file_content.readlines()
+        todos = get_todos()
         
         # Enumerate function generate indexing for a list
         for index, item in enumerate(todos):
@@ -28,8 +32,7 @@ while True:
             number = int(user_action[5:])
             number = number - 1
             
-            with open('todo/todos.txt', 'r') as file_content:
-                todos = file_content.readlines()
+            todos = get_todos()
                 
             new_todo = input("Enter new todo: ")
             todos[number] =  new_todo + "\n"
@@ -44,8 +47,8 @@ while True:
         try:
             number = int(user_action[9:])
             
-            with open('todo/todos.txt', 'r') as file_content:
-                todos = file_content.readlines()
+            todos = get_todos()
+            
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
