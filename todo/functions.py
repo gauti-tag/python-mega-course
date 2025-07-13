7083fe9c1026e9288399fc25014240ce30beaf5b
gautitag <gautier.tiehoule@gmail.com>
2025-07-13 13:47:49 +0000

    Style: Build a Todo List App | #local-moduole | Organizing the code in module

---
+++ todo/functions.py
@@ -0,0 +1,14 @@
+def get_todos(filepath = 'todo/todos.txt'):
+    """ 
+        Read a text file and return the list of to-do items.
+    """
+    with open(filepath, 'r') as file_content_local:
+        todos_local = file_content_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filpath = 'todo/todos.txt'):
+    """ write the to-do items list in the text file """
+    with open(filpath, 'w') as file_content_local:
+        file_content_local.writelines(todos_arg)
+
--- todo/main.py
+++ todo/main.py
@@ -1,18 +1,5 @@
-def get_todos(filepath = 'todo/todos.txt'):
-    """ 
-        Read a text file and return the list of to-do items.
-    """
-    with open(filepath, 'r') as file_content_local:
-        todos_local = file_content_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filpath = 'todo/todos.txt'):
-    """ write the to-do items list in the text file """
-    with open(filpath, 'w') as file_content_local:
-        file_content_local.writelines(todos_arg)
-
-
+#from functions import get_todos, write_todos
+import functions
 while True:
     # Get user input and strip space chars from it
     user_action = input("Type add, show, edit, complete or exit: ").strip()
@@ -22,15 +9,15 @@ while True:
         todo = user_action[4:]
         #Fetch the existing todos list        
         
-        todos = get_todos()
+        todos = functions.get_todos()
         
         todos.append(todo + '\n')
         
-        write_todos(todos)
+        functions.write_todos(todos)
             
     elif user_action.startswith("show"):
         #Fetch data from file    
-        todos = get_todos()
+        todos = functions.get_todos()
         
         # Enumerate function generate indexing for a list
         for index, item in enumerate(todos):
@@ -41,12 +28,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
             
-            todos = get_todos()
+            todos = functions.get_todos()
                 
             new_todo = input("Enter new todo: ")
             todos[number] =  new_todo + "\n"
             
-            write_todos(todos)
+            functions.write_todos(todos)
     
         except ValueError:
             print('Your command is not valid')
@@ -56,13 +43,13 @@ while True:
         try:
             number = int(user_action[9:])
             
-            todos = get_todos()
+            todos = functions.get_todos()
             
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
             
-            write_todos(todos)
+            functions.write_todos(todos)
                 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
